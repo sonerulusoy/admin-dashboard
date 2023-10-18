ffa06fd21638ff6f327123b6315cf89750ce948f
Soner Ulusoy <ulusoysoner0@outlook.com>
2023-10-19 01:22:39 +0300

    first commit

---
+++ client/src/scenes/geography/index.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { useGetGeographyQuery } from "state/api";
+import Header from "components/Header";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { geoData } from "state/geoData";
+
+const Geography = () => {
+    const theme = useTheme();
+    const { data } = useGetGeographyQuery();
+
+    return (
+        <Box m="1.5rem 2.5rem">
+            <Header title="GEOGRAPHY" subtitle="Kullanıcıların konumunun nerede olduğunu görebilirsiniz." />
+            <Box
+                mt="40px"
+                height="75vh"
+                border={`1px solid ${theme.palette.secondary[200]}`}
+                borderRadius="4px"
+            >
+                {data ? (
+                    <ResponsiveChoropleth
+                        data={data}
+                        theme={{
+                            axis: {
+                                domain: {
+                                    line: {
+                                        stroke: theme.palette.primary[100],
+                                    },
+                                },
+                                legend: {
+                                    text: {
+                                        fill: theme.palette.primary[100],
+                                    },
+                                },
+                                ticks: {
+                                    line: {
+                                        stroke: theme.palette.primary[100],
+                                        strokeWidth: 0.9,
+                                    },
+                                    text: {
+                                        fill: theme.palette.primary[100],
+                                    },
+                                },
+                            },
+                            legends: {
+                                text: {
+                                    fill: theme.palette.primary[100],
+                                },
+                            },
+                            tooltip: {
+                                container: {
+                                    color: theme.palette.primary.main,
+                                },
+                            },
+                        }}
+                        features={geoData.features}
+                        margin={{ top: 0, right: 0, bottom: 0, left: -50 }}
+                        domain={[0, 100]}
+                        unknownColor="#666666"
+                        label="properties.name"
+                        valueFormat=".1s"
+                        projectionScale={150}
+                        projectionTranslation={[0.5, 0.5]}
+                        projectionRotation={[0, 0, 0]}
+                        borderWidth={1}
+                        borderColor="#cccccc"
+                        legends={[
+                            {
+                                anchor: "bottom-left",
+                                direction: "column",
+                                justify: true,
+                                translateX: 100,
+                                translateY: -180,
+                                itemsSpacing: 5,
+                                itemWidth: 125,
+                                itemHeight: 25,
+                                itemDirection: "left-to-right",
+                                itemTextColor: theme.palette.primary[100],
+                                itemOpacity: 1,
+                                symbolSize: 25,
+                                effects: [
+                                    {
+                                        on: "hover",
+                                        style: {
+                                            itemTextColor: theme.palette.background.alt,
+                                            itemOpacity: 1,
+                                        },
+                                    },
+                                ],
+                            },
+                        ]}
+                    />
+                ) : (
+                    <>Loading...</>
+                )}
+            </Box>
+        </Box>
+    );
+};
+
+export default Geography;
---
+++ client/src/scenes/transactions/index.jsx
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useGetTransactionsQuery } from "state/api";
+import Header from "components/Header";
+import DataGridCustomToolbar from "components/DataGridCustomToolbar";
+
+const Transactions = () => {
+  const theme = useTheme();
+
+  // values to be sent to the backend
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+  const [searchInput, setSearchInput] = useState("");
+  const { data, isLoading } = useGetTransactionsQuery({
+    page,
+    pageSize,
+    sort: JSON.stringify(sort),
+    search,
+  });
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="TRANSACTIONS" subtitle="Entire list of transactions" />
+      <Box
+        height="80vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.transactions) || []}
+          columns={columns}
+          rowCount={(data && data.total) || 0}
+          rowsPerPageOptions={[20, 50, 100]}
+          pagination
+          page={page}
+          pageSize={pageSize}
+          paginationMode="server"
+          sortingMode="server"
+          onPageChange={(newPage) => setPage(newPage)}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+          components={{ Toolbar: DataGridCustomToolbar }}
+          componentsProps={{
+            toolbar: { searchInput, setSearchInput, setSearch },
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Transactions;
+
+/*
+// React ve gerekli bileşenlerin import edildiği kısım
+import React, { useState } from "react";
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useGetTransactionsQuery } from "state/api";
+import Header from "components/Header";
+import DataGridCustomToolbar from "components/DataGridCustomToolbar";
+
+// Transactions bileşeni
+const Transactions = () => {
+  // Tema değişkeninin kullanımı
+  const theme = useTheme();
+
+  // Backend'e gönderilecek değerlerin state'leri
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+  // Arama girişinin state'i
+  const [searchInput, setSearchInput] = useState("");
+
+  // Backend'den veri almak için kullanılan custom hook'un kullanımı
+  const { data, isLoading } = useGetTransactionsQuery({
+    page,
+    pageSize,
+    sort: JSON.stringify(sort),
+    search,
+  });
+
+  // DataGrid için sütunların belirtildiği kısım
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    // ... diğer sütunlar
+  ];
+
+  // JSX içinde oluşturulan bileşenler ve stillendirmeler
+  return (
+    // ... JSX içindeki Box ve DataGrid bileşenleri
+  );
+};
+
+// Transactions bileşeninin dışa aktarılması
+export default Transactions;
+
+*/
